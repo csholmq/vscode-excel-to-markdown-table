8d91e336dc4b00dbc988d9149d2e2d25028eb3b1
Des Horsley <deshorsley@hotmail.com>
2018-09-22 21:21:48 +1000

    Adds unit test for columnWidth

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -14,9 +14,10 @@ import * as myExtension from '../extension';
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("columnWidth", () => {
+        const row = [["first", "second"]];
+
+        assert.equal(myExtension.columnWidth(row, 0), 5);
+        assert.equal(myExtension.columnWidth(row, 1), 6);
     });
 });
