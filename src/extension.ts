15adfc96502896e340050e989c2657f7a7e47f0f
Tyler Haigh <tylerhaigh1994@me.com>
2018-09-23 16:24:45 +1000

    Move require clipboard back to methods

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {excelToMarkdown} from './excel-markdown-tables'
-import {clipboard} from "copy-paste";
 
 
 /**
@@ -12,10 +11,11 @@ import {clipboard} from "copy-paste";
  * This extension will run when the editor's language is set to 'markdown'.
  * The extension can be called through Ctrl-Shift-P and selecting the XXX Option,
  * or by using the shortcut Shift-Alt-V
- * @param context 
+ * @param context
  */
 export function activate(context: vscode.ExtensionContext) {
     var disposable = vscode.commands.registerCommand('extension.excelToMarkdown', () => {
+        let clipboard = require('copy-paste');
 
         clipboard.paste(function(err, val) {
             pasteText(val);
@@ -37,6 +37,7 @@ export function deactivate() {
  * @param rawData The raw clipboard contents containing the excel table to convert
  */
 function pasteText(rawData: string) {
+    let clipboard = require('copy-paste');
 
     let paste = excelToMarkdown(rawData);
 
